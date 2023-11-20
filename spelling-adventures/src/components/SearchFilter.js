a34073b0ef51177d60bf1d854f7e7efc4165f721
JunnaC <junnacao@163.com>
2023-11-19 17:57:32 -0800

    fix searchfilter

--- spelling-adventures/src/components/SearchFilter.js
+++ spelling-adventures/src/components/SearchFilter.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import TAGS_DATA from './data/tags.json';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export function SearchFilter(props) {
     const [isFilterOpen, setIsFilterOpen] = useState(true);
-
-    const tagArray = TAGS_DATA.map((tagObj) => {
-        const transform = <p key={tagObj.id}><input type="checkbox" id={tagObj.id}/><label htmlFor={tagObj.id}> {tagObj.word}</label></p>
-        return transform;
-    });
+    const [selectedTags, setSelectedTags] = useState([]);
 
     const toggleFilter = () => {
         setIsFilterOpen(!isFilterOpen);
+    };
+
+    const handleTagChange = (tag) => {
+        const updatedTags = selectedTags.includes(tag)
+            ? selectedTags.filter((selectedTag) => selectedTag !== tag)
+            : [...selectedTags, tag];
+        setSelectedTags(updatedTags);
     };
 
     return (
@@ -19,24 +23,36 @@ export function SearchFilter(props) {
             <div className="bars">
                 <div className='search'>
                     <span className="material-icons search-icon" aria-hidden="true">search</span>
-                    <input type="search-box" placeholder="Search words" aria-label="search" />
+                    <input type="search-box" placeholder="Search List Title" aria-label="search" />
                 </div>
                 <div className='filter'>
-                    {isFilterOpen && (
-                        <div className="dropdown">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
                             <span className="material-icons filter-icon" aria-hidden="true">filter_alt</span>
-                            <select value={tagArray}>
-                                <option value="">Filter Tags</option>
-                                    {tagArray.map((tag) => (
-                                        <option key={tag} value={tag}>
-                                            {tag}
-                                        </option>
-                                    ))}
-                            </select>                        
-                        </div>
-                    )}
+                            Filter Tags
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {isFilterOpen && (
+                                <div className="dropdown-content">
+                                    <div>
+                                        {TAGS_DATA.map((tagObj) => (
+                                            <p key={tagObj.id}>
+                                                <input
+                                                    type="checkbox"
+                                                    id={tagObj.id}
+                                                    checked={selectedTags.includes(tagObj.word)}
+                                                    onChange={() => handleTagChange(tagObj.word)}
+                                                />
+                                                <label htmlFor={tagObj.id}> {tagObj.word}</label>
+                                            </p>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
-            </div>   
+            </div>
         </div>
     );
 }
